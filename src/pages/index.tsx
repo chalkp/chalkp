0603b01c87dbb23552465f897e44dfb4c02719af
Sorrawit Poomseetong <git@chalkp.com>
2024-06-06 23:06:03 +0700

    edit mobile view
    Signed-off-by: Sorrawit Poomseetong <git@chalkp.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,12 +20,10 @@ export default function Home() {
             <div className="pb-4 border-b-4 border-b-bkk-bright-green">
               <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8">
                 <div className="text-center xl:text-left order-2 xl:order-none">
-                  <h1 className="text-bkk-bright-green font-bold text-9xl">
-                    {t('home:name')}
-                  </h1>
-                  <h1 className="text-bkk-bright-green font-bold text-8xl">
-                    {t('home:surname')}
-                  </h1>
+                  <div className="text-bkk-bright-green font-bold xl:text-8xl text-6xl">
+                    <h1>{t('home:name')}</h1>
+                    <h1>{t('home:surname')}</h1>
+                  </div>
                   <div className="max-w-[420px] mb-9 font-normal">
                     <p className="font-ChulaCharasNew text-[20px]">
                       Computer Engineering Chulalongkorn University
@@ -37,7 +35,7 @@ export default function Home() {
                 </div>
                 <div className="order-1 xl:order-none mb-8 xl:mb-0">
                   <div className="w-full h-full relative">
-                    <div className="w-[420px] h-[420px] xl:w-[690px] xl:h-[690px]">
+                    <div className="w-[300px] h-[300px] xl:w-[690px] xl:h-[690px]">
                       <Image
                         src="/assets/profile_pic.png"
                         priority
