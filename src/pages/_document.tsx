1e54401d4c0dccc482a16c551b83774ec76e0e5f
Sorrawit Poomseetong <git@chalkp.com>
2024-06-06 22:49:43 +0700

    fix Head and &quot;
    Signed-off-by: Sorrawit Poomseetong <git@chalkp.com>

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,9 @@
-import { Html, Head, Main, NextScript } from "next/document"
+import { Head, Html, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Sorrawit Poomseetong</title>
-        <meta name="title" content="chalkp" />
-        <meta name="description" content="Sorrawit Poomseetong" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
